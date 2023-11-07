b0167fe5bfeb70a88901f86fade7ef67ab503aa6
Humoyun <abduodirovhumoyun@gmail.com>
2023-11-07 13:40:17 +0500

    90% catalogue

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,25 @@ import {
   Container,
   Divider,
   Drawer,
+  Grid,
   IconButton,
+  Typography,
 } from "@mui/material";
 import { AiOutlineHeart, AiOutlineSearch as SearchIcon } from "react-icons/ai";
-import { BiShoppingBag as BagIcon, BiSun } from "react-icons/bi";
+import {
+  BiShoppingBag as BagIcon,
+  BiSun,
+  BiSolidWasher,
+  BiChevronRight,
+  BiChevronDown,
+} from "react-icons/bi";
 import { MdOutlineDarkMode as DarkModeIcon } from "react-icons/md";
 import { FiList, FiMenu, FiUser, FiX } from "react-icons/fi";
 import { useState } from "react";
-
+import { SlEarphones } from "react-icons/sl";
+import { BsLaptop } from "react-icons/bs";
+import { PiBooksBold } from "react-icons/pi";
+import { catalogue } from "../../../api";
 export interface darkMode {
   isDarkMode: boolean;
   setIsDarkMode: (isDarkMode: boolean) => void;
@@ -29,6 +40,8 @@ export const Header = ({ isDarkMode, setIsDarkMode }: darkMode) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isListIcon, setIsListIcon] = useState(true);
   const [isCatalogOpen, setIsCatalogOpen] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [isNestedDrawerOpen, setIsNestedDrawerOpen] = useState(false);
 
   const toggleIcon = () => {
     setIsListIcon(!isListIcon);
@@ -40,6 +53,20 @@ export const Header = ({ isDarkMode, setIsDarkMode }: darkMode) => {
 
   const toggleCatalog = () => {
     setIsCatalogOpen(!isCatalogOpen);
+    // Sahifadagi scroll ni yopish
+    if (!isCatalogOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  };
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  const toggleNestedDrawer = () => {
+    setIsNestedDrawerOpen(!isNestedDrawerOpen);
   };
   return (
     <Box>
@@ -260,15 +287,169 @@ export const Header = ({ isDarkMode, setIsDarkMode }: darkMode) => {
         onClose={toggleCatalog}
         anchor="left"
         variant="persistent"
-        sx={{ zIndex: 5, [`& .MuiDrawer-paper`]: { top: "140px" }, display: { xs: "none", xl: "block" } }}
+        sx={{
+          zIndex: 5,
+          [`& .MuiDrawer-paper`]: { top: "140px", zIndex: 5 },
+          display: { xs: "none", xl: "block" },
+        }}
       >
         <Box
           sx={{
-            width: "270px",
+            width: "450px",
             padding: "20px",
           }}
-        ></Box>
+        >
+          {data.map((item, index) => (
+            <Box
+              onMouseEnter={toggleNestedDrawer}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 2,
+                justifyContent: "space-between",
+                my: 1,
+                px: 1,
+                borderRadius: 1,
+                cursor: "pointer",
+                backgroundColor:
+                  index === activeIndex ? "#8c8c8c" : "transparent",
+                "&:hover": { backgroundColor: "#8c8c8c" },
+              }}
+              key={index}
+              onClick={() => {
+                handleClick(index), toggleNestedDrawer();
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                <Typography sx={{ fontSize: "28px" }}>{item.icon}</Typography>
+                <Typography fontWeight={600}>{item.text}</Typography>
+              </Box>
+              <BiChevronRight style={{ fontSize: "28px" }} />
+            </Box>
+          ))}
+        </Box>
       </Drawer>
+      {isCatalogOpen && (
+        <Drawer
+          open={isNestedDrawerOpen}
+          onClose={() => {
+            toggleNestedDrawer();
+            toggleCatalog();
+          }}
+          anchor="left"
+          variant="persistent"
+          sx={{
+            width: 300,
+            position: "absolute",
+            [`& .MuiDrawer-paper`]: {
+              left: "450px",
+              top: "140px",
+              zIndex: 4,
+              width: "100%",
+              display: { xs: "none", xl: "block" },
+            },
+          }}
+        >
+          <Box
+            sx={{
+              width: "1000px",
+              padding: "20px",
+              overflowY: "auto", // Pastga scroll qilish
+              maxHeight: "80vh",
+              "&::-webkit-scrollbar": {
+                width: 0,
+                background: "transparent",
+              },
+            }}
+          >
+            <Grid container spacing={2} direction="row" alignItems="center">
+              {catalogue.map((item, index) => (
+                <Grid item xl={4} key={index}>
+                  <Typography fontWeight={600}>{item.name}</Typography>
+                  <Box
+                    sx={{
+                      mt: 1,
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: 0.5,
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        "&:hover": {
+                          textDecoration: "underline",
+                          fontWeight: "500",
+                        },
+                      }}
+                    >
+                      {item.text1}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        "&:hover": {
+                          textDecoration: "underline",
+                          fontWeight: "500",
+                        },
+                      }}
+                    >
+                      {item.text2}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        "&:hover": {
+                          textDecoration: "underline",
+                          fontWeight: "500",
+                        },
+                      }}
+                    >
+                      {item.text3}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        "&:hover": {
+                          textDecoration: "underline",
+                          fontWeight: "500",
+                        },
+                      }}
+                    >
+                      {item.text4}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        cursor: "pointer",
+                        display: "flex",
+                        alignItems: "center",
+                        color: "gray",
+                      }}
+                    >
+                      Ещё <BiChevronDown />
+                    </Typography>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Drawer>
+      )}
     </Box>
   );
 };
+
+const data = [
+  {
+    icon: <SlEarphones />,
+    text: "*ЭЛЕКТРОНИКА",
+  },
+  {
+    icon: <BsLaptop />,
+    text: "*НОУТБУКИ И КОМПЬЮТЕРЫ",
+  },
+  {
+    icon: <BiSolidWasher />,
+    text: "*БЫТОВАЯ ТЕХНИКА",
+  },
+  {
+    icon: <PiBooksBold />,
+    text: "*КНИГИ",
+  },
+];
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ import { Header } from "./Header/Header";
 import { Hero } from "./Hero/Hero";
 import Slider from "./Slider/Slider";
 import { Footer } from "./Footer/Footer";
+
 export { Layout, Header, Hero, Slider, Footer };
